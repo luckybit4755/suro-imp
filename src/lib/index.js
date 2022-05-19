fd789d190751ed459392e3b554ce926d4c9c4318
vgvm <luckybit4755@gmail.com>
2022-05-19 18:48:11 -0400

    fix for babel and add index.js

---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+import { Multidimensional } from './model/Multidimensional.js';
+import { Rules            } from './model/Rules.js';
+import { Tile             } from './model/Tile.js';
+import { Ruler            } from './suroimp/Ruler.js';
+import { Smasher          } from './suroimp/Smasher.js';
+
+export { Multidimensional, Rules, Tile, Ruler, Smasher };
